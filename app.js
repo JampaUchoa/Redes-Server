dbaf7959150af1f5518a58cd16693bdf1547a5b1
Jampa Uchoa <joaopcu@gmail.com>
2019-07-05 15:10:16 -0300

    done

--- app.js
+++ app.js
@@ -6,26 +6,10 @@ const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'files')));
 
-app.post('*', function (req, res) {
-  treat400(res)
-})
-
-app.put('*', function (req, res) {
-  treat400(res)
-})
-
-app.delete('*', function (req, res) {
-  treat400(res)
-})
-
 // Handles any requests that don't match the ones above
 app.use(function (req, res, next) {
   res.status(404).send("Desculpe, mas não conseguimos encontrar a pagina solicitada")
 })
-
-function treat400(res){
-  return res.status(400).send('400: Parado aí mesmo escoria criminal!\n');
-}
 
 app.use(function (err, req, res, next) {
   console.error(err.stack)
--- client/get.js
+++ client/get.js
@@ -1,16 +1,20 @@
 const request = require('request');
-const chalk = require('chalk');
+var marked = require('marked');
+var TerminalRenderer = require('marked-terminal');
 
-request('http://localhost:3000/file.htm', (err, res, body) => {
+url = `http://${process.env.ip || "10.18.50.5"}:${process.env.port || "80"}/${process.env.URL}`
+console.log(url);
+
+request(url, (err, res, body) => {
     if (err) { return console.log(err); }
-    //console.log(body);
-    console.log(chalk.bold('Hello!'));
-    console.log(chalk`{bold.green hello}`);
-
-    let reg = /<(([a-z]+)\s*([^>]*))>/gms;
-
-    let result = body.match(reg);
-    console.log(result); // <span class="my">, span class="my", span, class="my"
-
+    
+    marked.setOptions({
+        // Define custom renderer
+        renderer: new TerminalRenderer()
+    });
+    
+    console.log(body)
+    // Show the parsed data
+    console.log(marked(body));
 
 });
